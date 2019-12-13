cc8651ef564de078e43ff312e47e41562eaaf99c
ravikishore <ravikishore1979@gmail.com>
2019-12-13 19:41:57 +0530

    - removed rm and mkdir statements.

--- src/main/java/com/creactiviti/piper/workflows/model/JenkinsJobTask.java
+++ src/main/java/com/creactiviti/piper/workflows/model/JenkinsJobTask.java
@@ -23,7 +23,7 @@ public class JenkinsJobTask extends WorkflowTask {
             "    \"surroundWithTryCatch\" : true,\n" +
             "    \"statementList\" : [ {\n" +
             "      \"type\" : \"gst\",\n" +
-            "      \"statment\" : \" sh '''\\n        rm -rf ${JENKINS_HOME}/workspace/${JOB_NAME}/*\\n        mkdir -p ${JENKINS_HOME}/workspace/${JOB_NAME}\\n        cp -ra \\\"${JENKINS_HOME}/workspace/${BUILD_JOB_NAME}/${BUILD_JOB_NUMBER}/.\\\"  \\\"${JENKINS_HOME}/workspace/${JOB_NAME}/\\\"\\n        '''\"\n" +
+            "      \"statment\" : \" sh '''\\n        cp -ra \\\"${JENKINS_HOME}/workspace/${params.BUILD_JOB_NAME}/${params.BUILD_JOB_NUMBER}/.\\\"  \\\"${JENKINS_HOME}/workspace/${JOB_NAME}/\\\"\\n        '''\"\n" +
             "    }, {\n" +
             "      \"type\" : \"gst\",\n" +
             "      \"statment\" : \"writeFile file:'.pipeline/config.yaml', text: \\\"${params.configyaml}\\\"\"\n" +
