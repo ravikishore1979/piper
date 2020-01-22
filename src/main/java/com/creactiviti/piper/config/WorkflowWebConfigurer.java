43e94cd9028278e19ea08b3957663cef0b628aed
ravikishore <ravikishore1979@gmail.com>
2020-01-22 14:37:20 +0530

    - Added a log statement to display the Cors domains

--- src/main/java/com/creactiviti/piper/config/WorkflowWebConfigurer.java
+++ src/main/java/com/creactiviti/piper/config/WorkflowWebConfigurer.java
@@ -1,11 +1,13 @@
 package com.creactiviti.piper.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Slf4j
 @Configuration
 @EnableWebMvc
 public class WorkflowWebConfigurer extends WebMvcConfigurerAdapter {
@@ -15,6 +17,7 @@ public class WorkflowWebConfigurer extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("ORIGINS: {}", origins);
         registry.addMapping("/**").allowedOrigins(origins);
     }
 }
